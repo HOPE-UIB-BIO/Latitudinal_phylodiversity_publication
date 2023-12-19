1f703c9efb876c1c63ea94c52b9bc1f89185c8cd
Kuber Bhatta <47561250+kbh022@users.noreply.github.com>
2023-12-19 18:19:13 +0100

    function for estimating phylodiversity

--- R/functions/get_phylogenetic_diversity.R
+++ R/functions/get_phylogenetic_diversity.R
@@ -2,24 +2,24 @@
 # Function for estimating phylogenetic diversity ----
 #-----------------------------------------------#
 
-get_phylogenetic_diversity <- function(counts, 
-                                       dataset_id,
-                                       type = "mpd",
-                                       null.model = "phylogeny.pool", 
-                                       abundance.weighted = TRUE,
-                                       runs = 99, 
-                                       ...) {
-  
-  message(
+get_phylogenetic_diversity <- 
+  function(counts,
+           dataset_id,
+           type = "mpd",
+           null.model = "phylogeny.pool", 
+           abundance.weighted = TRUE,
+           runs = 99,
+           ...) {
+    message(
     msg = dataset_id
-  )
+    )
   
   dat <- 
     counts %>%
     as.data.frame() %>% 
     column_to_rownames("sample_id")
   
-  tree <- 
+  final_tree <- 
     ape::read.tree(
       paste(
         "Inputs/Data/Ramirez_Barahona_etal_2020_phylogeny/",
@@ -30,11 +30,11 @@ get_phylogenetic_diversity <- function(counts,
   
   # Make a vector of families from the tree that are missing from the counts data
   drop_list <- 
-    tree$tip.label[!tree$tip.label %in% colnames(dat)]
+    final_tree$tip.label[!final_tree$tip.label %in% colnames(dat)]
   
   # Remove all the families that do not occur in our sample using drop.tip() 
   #  function in the 'ape' package.
-  pruned_tree <- ape::drop.tip(tree, drop_list) 
+  pruned_tree <- ape::drop.tip(final_tree, drop_list) 
   
   # Arrange taxa in the same order as the pruned tree
   data_ordered <- dat[,c(pruned_tree$tip.label)]
@@ -46,16 +46,14 @@ get_phylogenetic_diversity <- function(counts,
   # MPD using ses.mpd() function in the 'picante' package
   
   if (type == "mpd") {
-    
-    
     mpd_phylogeny <- 
       picante::ses.mpd(
         data_ordered,
         phy_dist,
         null.model = null.model, 
         abundance.weighted = abundance.weighted,
-        runs = runs
-      ) %>% 
+        runs
+        ) %>% 
       rownames_to_column("sample_id") %>% 
       as_tibble() 
     
@@ -70,7 +68,7 @@ get_phylogenetic_diversity <- function(counts,
         null.model = null.model, 
         abundance.weighted = abundance.weighted,
         runs = runs
-      ) %>% 
+        ) %>% 
       rownames_to_column("sample_id") %>% 
       as_tibble() 
     
